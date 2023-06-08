c2f97535cf62f9b5210a2b021b68b15ec5cc92ee
nxcmfg <haydenmccarthy19@gmail.com>
2023-06-08 11:01:55 -0400

    Fix sort order

--- models/nxc_mql_priority_opportunity.py
+++ models/nxc_mql_priority_opportunity.py
@@ -2,7 +2,6 @@ from odoo import api, fields, models
 
 class Lead(models.Model):
     _inherit = 'crm.lead'
-
     _order = "mql_priority desc, priority desc, id desc"
     
     #MQL Score Items
@@ -13,19 +12,11 @@ class Lead(models.Model):
         ('3','MQL 21-25'),
     ], string="MQL Priority")
 
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        stage_id = self.env.context.get('default_stage_id')
-        if stage_id == '1':
-            if not order or 'mql_priority' not in order:
-                order = 'mql_priority desc, ' + self._order if self._order else 'mql_priority desc'
-        return super(Lead, self).search(args, offset=offset, limit=limit, order=order, count=count)
-
     @api.onchange('stage_id', 'partner_id', 'partner_id.mql_score')
     def _compute_mql_priority(self):
         # Calculate the MQL Priority based on the value of MQL Score field from partner.
         for lead in self:
-            if lead.stage_id == '1':
+            if lead.stage_id.id == '1':
                 if lead.partner_id.mql_score < 11:
                     lead['mql_priority'] = '0'
                 elif 11 <= lead.partner_id.mql_score <= 15:
